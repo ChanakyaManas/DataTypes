1d2f1a4373b0e7e8ae2ce0429c9c1969274da1c7
Aparna526 <aparnakondapaturu@gmail.com>
2024-08-13 10:03:36 +0530

    Two

--- Test.java
+++ Test.java
@@ -20,7 +20,25 @@ public class Test{
         student1.status=in.nextBoolean();
 
 
-        System.out.println("id:"+student1.id+"Name:"+student1.Name+"Mobile no:"+student1.mno);
+        System.out.println("id:"+student1.id + "Name:"+student1.Name + "Mobile no:"+student1.mno);
+
+
+        System.out.println("Enter Student id");
+        student2.id=in.nextInt();
+        System.out.println("Enter Student name");
+        student2.Name=in.next();
+        System.out.println("Enter Student mobile number");
+        student2.mno=in.nextLong();
+        System.out.println("Enter Student CGPA");
+        student2.cgpa=in.nextFloat();
+        System.out.println("Enter Student score");
+        student2.score=in.nextInt();
+        System.out.println("Enter Student status");
+        student2.status=in.nextBoolean();
+
+
+        System.out.println("id:"+student2.id + "Name:"+student2.Name + "Mobile no:"+student2.mno);
+
 
     }
 }
