d7d3d99ed226ca5960c8b558ff2922f5888845f0
Revathi <revathivinnakota90@gmail.com>
2024-08-13 10:47:40 +0530

    revathi

--- Test.java
+++ Test.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class Test{
+    public static void main(String[] args) {
+        Student s=new Student();
+        Scanner in=new Scanner(System.in);
+        s.id=in.nextInt();
+        s.name=in.next();
+        System.out.println(s.id+","+s.name);
+    }
 }
