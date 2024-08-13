82f48f42e097c67cac3924a1741114adb8953238
Vishalprepro <adepuvishal17@gmail.com>
2024-08-13 09:53:34 +0530

    assignment

---
+++ project.java
@@ -0,0 +1,13 @@
+
+public class project {
+
+    int batch;
+    short score;
+    double average;
+    float gpa;
+    String name;
+    char gender;
+    byte rollnumber;
+    boolean completed;
+
+}
