5914259e76d53b29041d2db8959354f3cd6b5b01
Shoebmirza00 <shoebbaig31@gmail.com>
2024-08-13 10:00:27 +0530

    assignment

---
+++ studentshoeb.java
@@ -0,0 +1,12 @@
+public class studentshoeb{
+    int id;
+    String name;
+    char gender;
+    float marks;
+    long num;
+    double data;
+    boolean pass;
+    byte age;
+    short weight;
+
+}
